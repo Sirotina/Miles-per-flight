acaf0e7307c5ceadc62f146cf741959fd744d4ee
Neto test <o.sirotina@inbox.ru>
2023-01-16 14:37:45 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Стоимость билета //
+        int x = 10_000;
+
+        //  количество рублей для одной бонусной мили //
+
+        int m = 20;
+
+        int a = x / m;
+        
+        System.out.println( x / m );
+
+
+
+
+
+    }
+}
