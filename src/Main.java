8adc6b1954265b19b13461b9b3245c6bb341e981
Neto test <o.sirotina@inbox.ru>
2023-01-19 11:38:36 +0300

    Change Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int ticket_price = 10_000;
+        int ticketPrice = 10_000;
 
         int bonus = 20;
 
-        int amount = ticket_price / bonus ;
+        int amount = ticketPrice / bonus ;
         
         System.out.println("Количество начисленных миль: " + bonus);
 
